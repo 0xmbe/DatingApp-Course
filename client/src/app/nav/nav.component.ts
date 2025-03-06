fe0f56b47482e435d8b9bc728d91f12c13a4bda8
M <M@M>
2025-03-06 13:15:58 +0100

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,28 +3,34 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 //import { NgIf } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   //loggedIn = false;
   model: any = {};
 
   login(){
     //console.log(this.model);
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        //this.loggedIn = true;
+      next: () => {
+        //console.log(response);
+        this.router.navigateByUrl('/members');
       },
       error: error => {
-        console.log(error);
+        //console.log(error);
+        this.toastr.error(error.error);
       }
     })
   }
@@ -32,5 +38,6 @@ export class NavComponent {
   logout(){
     //this.loggedIn = false;
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
